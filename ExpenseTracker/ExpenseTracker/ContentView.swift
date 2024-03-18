5179f3ac0c63a606087e005b62ef1ffa5a3b121b
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-03-18 19:37:23 +0600

    Initialised Projects

---
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by Shahwat Hasnaine on 18/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Shahwat Hasnaine on 18/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
