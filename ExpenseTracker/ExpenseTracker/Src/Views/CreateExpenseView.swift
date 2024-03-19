6e196141da8491af09a544c843cafce0437845ce
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-03-19 16:03:13 +0600

    Structured the files

---
+++ ExpenseTracker/ExpenseTracker/Src/Views/CreateExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateExpenseView.swift
+//  ExpenseTracker
+//
+//  Created by Shahwat Hasnaine on 19/3/24.
+//
+
+import SwiftUI
+
+struct CreateExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateExpenseView()
+}
