3c06284bab2d0291e31d56c05577e8cbe9d9c551
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-03-19 18:58:53 +0600

    Implemented Form to add new expenses

--- ExpenseTracker/ExpenseTracker/Src/Views/CreateExpenseView.swift
+++ ExpenseTracker/ExpenseTracker/Src/Views/CreateExpenseView.swift
@@ -6,11 +6,53 @@
 //
 
 import SwiftUI
+import Combine
 
 struct CreateExpenseView: View {
+    
+    @State var title: String = ""
+    @State var description: String = ""
+    @State var category: String = ""
+    
+    @State var amount: String = ""
+    @State var type: ExpenseType = .random
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section("Expense Info") {
+                TextField("Title", text: $title)
+                TextField("Description", text: $description)
+            }
+            
+            Section("Category") {
+                TextField("Category", text: $category)
+            }
+            
+            Section("Expense Details") {
+                TextField("Amount (in taka)", text: $amount)
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(amount)) { newValue in
+                        let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            self.amount = filtered
+                        }
+                    }
+                Picker("Expense Type", selection: $type) {
+                    Text(ExpenseType.random.rawValue)
+                    Text(ExpenseType.recurrent.rawValue)
+                }
+            }
+            
+            Section {
+                Button {
+                    
+                } label: {
+                    Text("Create Expense")
+                }
+            }
+        }
     }
+  
 }
 
 #Preview {
