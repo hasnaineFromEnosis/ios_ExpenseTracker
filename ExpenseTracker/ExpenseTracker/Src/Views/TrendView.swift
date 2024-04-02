5092342e9cad8d100a0bc51f0b6a70bfe3abaf49
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-04-03 01:56:37 +0600

    Separated CustomDatePickerView

--- ExpenseTracker/ExpenseTracker/Src/Views/TrendView.swift
+++ ExpenseTracker/ExpenseTracker/Src/Views/TrendView.swift
@@ -7,53 +7,6 @@
 
 import SwiftUI
 
-import SwiftUI
-
-struct MonthPicker: View {
-    @Binding var month: Int
-    @Environment(\.calendar) var calendar
-    
-    var body: some View {
-        Picker("", selection: $month) {
-            ForEach(1..<13) { i in
-                Text(calendar.monthSymbols[i-1]).tag(i)
-            }
-        }
-        .pickerStyle(MenuPickerStyle())
-        .foregroundColor(.black) // Text color
-        .frame(width: 150) // Set width
-        .padding(.horizontal, 10) // Adjust padding
-        .background(
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundColor(.white) // Background color
-                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2) // Add shadow
-        )
-    }
-}
-
-struct YearPicker: View {
-    @Binding var year: Int
-    let startYear: Int = 2000
-    let endYear: Int = 2030
-    
-    var body: some View {
-        Picker("", selection: $year) {
-            ForEach(startYear...endYear, id: \.self) { year in
-                Text(String(year)).tag(year)
-            }
-        }
-        .pickerStyle(MenuPickerStyle())
-        .foregroundColor(.black) // Text color
-        .frame(width: 100) // Set width
-        .padding(.horizontal, 10) // Adjust padding
-        .background(
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundColor(.white) // Background color
-                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2) // Add shadow
-        )
-    }
-}
-
 struct TrendView: View {
     @State private var selectedMonth = 1
     @State private var selectedYear = 2022
@@ -64,7 +17,6 @@ struct TrendView: View {
                 MonthPicker(month: $selectedMonth)
                 YearPicker(year: $selectedYear)
             }
-            
             Spacer()
         }
     }
