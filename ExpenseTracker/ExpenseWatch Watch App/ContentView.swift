7c0c0ca8695ac75c8fa5907272390d61e9a71929
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-05-06 12:01:44 +0600

    Init WatchOS

---
+++ ExpenseTracker/ExpenseWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseWatch Watch App
+//
+//  Created by Shahwat Hasnaine on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTracker/ExpenseWatch Watch App/ExpenseWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseWatchApp.swift
+//  ExpenseWatch Watch App
+//
+//  Created by Shahwat Hasnaine on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
