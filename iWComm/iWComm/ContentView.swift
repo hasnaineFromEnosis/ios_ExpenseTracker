6e2290dbccf839550b538aadb6f8aa59a5ab4f26
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-05-10 02:14:21 +0600

    Init Rnd Project

---
+++ iWComm/iWComm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iWComm
+//
+//  Created by Shahwat Hasnaine on 10/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iWComm/iWComm/iWCommApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWCommApp.swift
+//  iWComm
+//
+//  Created by Shahwat Hasnaine on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iWCommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
